c85655dfcd9c0def3e462132f7798b81374ded70
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-04-19 20:16:16 +0200

    Added comments

--- assert.go
+++ assert.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 )
 
+// messageFromMsgAndArgs will return the message
+// if it exists.
 func messageFromMsgAndArgs(msgAndArgs ...interface{}) string {
 	if len(msgAndArgs) == 0 || msgAndArgs == nil {
 		return ""
@@ -21,6 +23,7 @@ func messageFromMsgAndArgs(msgAndArgs ...interface{}) string {
 	return ""
 }
 
+// equal will test if the expected and actual value is the same match.
 func equal(expected, actual interface{}) bool {
 	if expected == nil || actual == nil {
 		return expected == actual
@@ -37,6 +40,7 @@ func equal(expected, actual interface{}) bool {
 	return false
 }
 
+// Fail will print a failing message to the terminal.
 func Fail(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}) {
 	message := messageFromMsgAndArgs(msgAndArgs...)
 	_, file, line, _ := runtime.Caller(1)
@@ -48,6 +52,7 @@ func Fail(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{})
 		actual)
 }
 
+// Equal will test if the expected and actual value is the same match.
 func Equal(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
 	if !equal(expected, actual) {
 		Fail(t, expected, actual, msgAndArgs...)
@@ -56,6 +61,7 @@ func Equal(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}
 	return true
 }
 
+// True will test the actual value and see if it's true or not.
 func True(t *testing.T, actual bool, msgAndArgs ...interface{}) bool {
 	if actual != true {
 		Fail(t, true, actual, msgAndArgs...)
@@ -64,6 +70,7 @@ func True(t *testing.T, actual bool, msgAndArgs ...interface{}) bool {
 	return true
 }
 
+// False will test the actual value and see if it's false or not.
 func False(t *testing.T, actual bool, msgAndArgs ...interface{}) bool {
 	if actual != false {
 		Fail(t, false, actual, msgAndArgs...)
@@ -72,26 +79,7 @@ func False(t *testing.T, actual bool, msgAndArgs ...interface{}) bool {
 	return true
 }
 
-func NotNil(t *testing.T, actual interface{}, msgAndArgs ...interface{}) bool {
-	success := true
-
-	if actual == nil {
-		success = false
-	} else {
-		value := reflect.ValueOf(actual)
-		kind := value.Kind()
-		if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
-			success = false
-		}
-	}
-
-	if !success {
-		Fail(t, nil, actual, msgAndArgs...)
-	}
-
-	return success
-}
-
+// Nil will test the actual value and see if it's nil or not.
 func Nil(t *testing.T, actual interface{}, msgAndArgs ...interface{}) bool {
 	success := true
 
@@ -103,6 +91,27 @@ func Nil(t *testing.T, actual interface{}, msgAndArgs ...interface{}) bool {
 	kind := value.Kind()
 	if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
 		success = false
+	}
+
+	if !success {
+		Fail(t, nil, actual, msgAndArgs...)
+	}
+
+	return success
+}
+
+// NotNil test check if the actual value is not nil.
+func NotNil(t *testing.T, actual interface{}, msgAndArgs ...interface{}) bool {
+	success := true
+
+	if actual == nil {
+		success = false
+	} else {
+		value := reflect.ValueOf(actual)
+		kind := value.Kind()
+		if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
+			success = false
+		}
 	}
 
 	if !success {
